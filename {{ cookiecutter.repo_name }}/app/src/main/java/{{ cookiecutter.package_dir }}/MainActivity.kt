48fd19cd8f486525a7d20424110ed7fc29710ffd
Richard Lucas <richard.lucas@raizlabs.com>
2018-10-10 15:07:02 -0700

    Edit import statements to androidx.

--- {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/MainActivity.kt
+++ {{ cookiecutter.repo_name }}/app/src/main/java/{{ cookiecutter.package_dir }}/MainActivity.kt
@@ -1,6 +1,6 @@
 package {{ cookiecutter.package_name }}
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
