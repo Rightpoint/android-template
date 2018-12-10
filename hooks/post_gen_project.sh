1b346ab000c398aff9e38a285792b7eb9688f392
jonathanduran <jonduran3000@gmail.com>
2018-12-10 10:17:54 -0500

    Updates to the post generation script
    * Checks for Homebrew and install or updates it
    * Installs ktlint
    * Applies ktlint formatting to the project

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,11 +4,24 @@ git init
 
 git add -A
 
+# Check for Homebrew, if it's not installed then install it
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew update
+fi
+
+brew install shyiko/ktlint/ktlint
+
+ktlint --apply-to-idea-project --android
+
 cd ..
 chmod -R 777 "{{ cookiecutter.repo_name }}"
 
-if [ -z ${CI+x} ]; then
-    if [ "{{ cookiecutter.launch_studio }}" = true ]; then
+if [[ -z ${CI+x} ]]; then
+    if [[ "{{ cookiecutter.launch_studio }}" = true ]]; then
         /Applications/Android\ Studio.app/Contents/MacOS/studio $PWD/"{{ cookiecutter.repo_name }}"
         echo "Android Studio should now be running, have fun with your new project!"
     else
