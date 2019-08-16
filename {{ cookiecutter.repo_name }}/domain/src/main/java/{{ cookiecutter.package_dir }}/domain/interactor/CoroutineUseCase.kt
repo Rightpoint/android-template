65fbbf91526fb60bdf09b6cbf77775dbdd2bd9de
Matt Ognibene <ognibene.m@husky.neu.edu>
2019-08-16 10:03:16 -0400

    changes per review

--- {{ cookiecutter.repo_name }}/domain/src/main/java/{{ cookiecutter.package_dir }}/domain/interactor/CoroutineUseCase.kt
+++ {{ cookiecutter.repo_name }}/domain/src/main/java/{{ cookiecutter.package_dir }}/domain/interactor/CoroutineUseCase.kt
@@ -1,25 +1,11 @@
 package {{ cookiecutter.package_name }}.domain.interactor
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import java.lang.Exception
-
 abstract class CoroutineUseCase<in T, out R> {
-    operator fun invoke(
-            scope: CoroutineScope,
-            params: T,
-            onResult: (Result<out R>) -> Unit = {}
-    ) {
-        val backgroundJob = scope.async {
-            try {
-                Result.success(execute(params))
-            } catch (e: Exception) {
-                Result.failure<R>(e)
-            }
-        }
-        scope.launch { onResult(backgroundJob.await()) }
+    suspend operator fun invoke(params: T): R {
+        return execute(params)
     }
 
     protected abstract suspend fun execute(params: T): R
 }
+
+suspend operator fun <R> CoroutineUseCase<Unit, R>.invoke(): R = this(Unit)
